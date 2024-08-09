8e9fc92bf535a747d433b0553a5638de1a15e5bb
Mateusz Moruś <mpmorus@gmail.com>
2024-08-09 12:03:05 +0200

    Day 14 (#54)
    * Solved part one
    
    * Solved part two
    
    * Added README summary
    
    * Move day 14 to solved solution folder

---
+++ Day14 - Parabolic Reflector Dish/Day14SolverOptions.cs
@@ -0,0 +1,8 @@
+using AdventOfCode.Abstractions;
+
+namespace AdventOfCode.Year2023.Day14;
+
+public sealed class Day14SolverOptions : DaySolverOptions
+{
+	public int PartTwoCycleCount { get; set; } = 1_000_000_000;
+}
---
+++ Day14 - Parabolic Reflector Dish/SpaceType.cs
@@ -0,0 +1,8 @@
+namespace AdventOfCode.Year2023.Day14;
+
+internal enum SpaceType
+{
+	Empty = 0,
+	CubeRock = 1,
+	RoundRock = 2,
+}
